b4e1060ca01cada9df677f812d701f9b9a437911
roziqinakhmad14 <roziqinakhmad14juli@gmail.com>
2024-10-28 11:10:20 +0700

    feat: finishing create feature

--- app.js
+++ app.js
@@ -3,36 +3,50 @@ const moment = require('moment')
 const bodyParser = require('body-parser')
 const db = require('./connection')
 const cors = require('cors')
+const uuid = require('uuid')
+const requestIp = require('request-ip')
 const app = express()
 const port = 3000
 
-let corsOptions = {
+moment.locale('id')
+
+const corsOptions = {
   origin: '*',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
 app.use(cors(corsOptions))
 app.use(bodyParser.json())
-
-moment.locale('id')
+app.use(requestIp.mw())
 
 app.get('/', (req, res) => {
-  db.query('SELECT * FROM comments', (error, result) => {
+  db.query('SELECT * FROM comments ORDER BY created_at DESC', (error, result) => {
     result.map(item => {
       item['created_at'] = moment.duration(moment(item['created_at']).diff(moment())).humanize() + ' lalu'
       item['updated_at'] = moment.duration(moment(item['updated_at']).diff(moment())).humanize() + ' lalu'
-    }
-    )
+    })
     res.status(200).json(result)
   })
 })
 
 app.post('/', (req, res) => {
-  const { name, presence, numPresence, comment, token } = req.body
-  const createdAt = moment().format('YYYY-MM-DD HH:mm:ss')
-  const updatedAt = moment().format('YYYY-MM-DD HH:mm:ss')
-  db.query(`INSERT INTO comments VALUES (NULL, '${name}', '${presence}', '${numPresence}', '${comment}', '${token}', '${createdAt}', '${updatedAt}')`, (error, result) => {
-    res.send(result)
+  const { name, presence, num_presence, comment } = req.body
+  const user_agent = req.headers['user-agent']
+  const created_at = moment().format('YYYY-MM-DD HH:mm:ss')
+  const updated_at = moment().format('YYYY-MM-DD HH:mm:ss')
+  const myuuid = uuid.v4()
+  const own = uuid.v4()
+  const ip = req.clientIp
+  db.query(`INSERT INTO comments VALUES (NULL, '1', '${name}', '${presence}', '${num_presence}', '${comment}', '${created_at}', '${updated_at}', '${myuuid}', '${ip}', '${user_agent}', NULL, '${own}', '0')`, (error, result) => {
+    res.status(201).json({
+      name: name,
+      presence: presence,
+      comment: comment,
+      uuid: myuuid,
+      own: own,
+      created_at: moment.duration(moment().diff(moment())).humanize() + ' lalu',
+      code: 201,
+    })
   })
 })
 
--- connection.js
+++ connection.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql')
-// const db = mysql.createConnection({
-//     host: 'localhost',
-//     user: 'root',
-//     password: '',
-//     database: 'kamu'
-// })
 const db = mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    port: '3306',
-    user: 'sql12741106',
-    password: 'Mr3GtpgWBh',
-    database: 'sql12741106'
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'kamu'
 })
+// const db = mysql.createConnection({
+//     host: 'sql12.freesqldatabase.com',
+//     port: '3306',
+//     user: 'sql12741106',
+//     password: 'Mr3GtpgWBh',
+//     database: 'sql12741106'
+// })
 
 module.exports = db
